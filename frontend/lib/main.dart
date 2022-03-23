f54551415f0d51e5e5e81f69f2a7bae49fea1299
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-03-23 16:02:38 -0400

    Add drawer with visual login button.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,22 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth_desktop/firebase_auth_desktop.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/widgets/drag_container.dart';
 import 'package:frontend/widgets/pdf_viewer.dart';
 import 'package:frontend/widgets/search_results.dart';
 import 'package:frontend/widgets/topbar.dart';
 import 'package:frontend/widgets/text_editor.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyBeUk01P9gT8SRl3SVMqKJBln1x_iIBk0E',
+    appId: '1:685635914796:web:482972591bbdbe2af09589',
+    messagingSenderId: '685635914796',
+    projectId: 'tabless-notes',
+  ));
   runApp(const MyApp());
 }
 
@@ -36,6 +47,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: const MenuDrawer(),
         backgroundColor: const Color(0xff1D1D20),
         body: SizedBox(
           width: MediaQuery.of(context).size.width,
@@ -55,6 +67,46 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           ),
+        ));
+  }
+}
+
+class MenuDrawer extends StatelessWidget {
+  const MenuDrawer({Key? key}) : super(key: key);
+
+  void signInWithGoogle() {
+    // GoogleAuthProvider provider = GoogleAuthProvider();
+    // FirebaseAuthDesktop.instance.signInWithPopup(provider);
+  }
+
+  void signOut() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+        backgroundColor: const Color(0xff1D1D20),
+        elevation: 4,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(children: [
+            ElevatedButton(
+              onPressed: signInWithGoogle,
+              style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, padding: const EdgeInsets.all(16)),
+              child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    Icon(Icons.login),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text("Sign in with Google",
+                        style: TextStyle(color: Colors.white))
+                  ]),
+            )
+          ]),
         ));
   }
 }
