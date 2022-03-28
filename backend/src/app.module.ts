ee54e5fdf8ba3bcbc6defad01ab23a65eb2b2764
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-03-27 22:31:57 -0400

    Test firebase with nestjs. Setup some routes.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
+import { DatabaseController } from './database/database.controller';
+import { DatabaseModule } from './database/database.module';
+import { DatabaseService } from './database/database.service';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin'
+import serviceAccount from './config/serviceAccountKey';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key,
+      }),
+      databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+    });
+  } catch (e) {
+    console.log(e)
+  }
+
   await app.listen(3000);
 }
 bootstrap();
