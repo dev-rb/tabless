a1073af4b6fcb3abe6defdd6f72afd9f31b90481
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-03-30 16:11:14 -0400

    Add endpoint for generating keywords.

---
+++ backend/src/api/api.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiService } from './api.service';
+
+@Controller('api')
+export class ApiController {
+
+    constructor(private readonly api: ApiService) { }
+
+    @Get('/')
+    getHello() {
+        return "Hello World";
+    }
+
+    @Post('/generate/keywords')
+    async generateKeywords(@Body('text') textToCheck: string): Promise<string[]> {
+        return await this.api.generateKeywords(textToCheck);
+    }
+}
---
+++ backend/src/api/api.service.ts
@@ -0,0 +1,25 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { OpenAIApi } from 'openai';
+
+const TEST_TEXT = "The Guggenheim, short for the Solomon R. Guggenheim museum, is a very famous art museum located at Fifth Avenue in New York City, and is very much known for its collection of impressionist, post-impressionist, modern, and contemporary art, which continues to grow with time. One of the museum’s current exhibitions, titled “Artistic License,” features the curated works for six curated artists and runs from  May 24 2019 January 12th 2020.";
+
+const EXTRACT_KEYWORDS_TEXT = "Generate search keywords for google from this text: \n\n"
+
+@Injectable()
+export class ApiService {
+
+    constructor(@Inject('OPENAI_CLIENT') private openaiClient: OpenAIApi) { }
+
+    async generateKeywords(textToCheck: string): Promise<string[]> {
+        const result = await this.openaiClient.createCompletion('text-ada-001', {
+            prompt: EXTRACT_KEYWORDS_TEXT + TEST_TEXT,
+            temperature: 0.3,
+            max_tokens: 60,
+            top_p: 1.0,
+            frequency_penalty: 0.8,
+            presence_penalty: 0.0
+        }).catch((err) => console.log(err));
+        return [];
+    }
+
+}
