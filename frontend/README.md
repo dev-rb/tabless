34abd0daf893989313d7ca64687e645f50a1cd76
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-02 16:56:54 -0400

    Change template for electron setup.

--- frontend/README.md
+++ frontend/README.md
@@ -1,1 +1,2 @@
 # Tabless Notes
+----
---
+++ frontend/packages/renderer/src/components/title-bar.tsx
@@ -0,0 +1,30 @@
+// import { ipcRenderer, remote } from 'electron';
+import * as React from 'react';
+// const {remote} = window.require('electron');
+
+// const currWindow = remote.getCurrentWindow();
+
+const TitleBar = () => {
+
+  const handleCloseWindow = () => {
+    // console.log(remote.app.getAppPath())
+    // currWindow.close();
+    // ipcRenderer.send('ipc-example');
+  }
+
+  const handleMinimizeWindow = () => {
+
+  }
+
+  const handleMaximizeWindow = () => {
+
+  }
+
+  return (
+    <nav className="title-bar-drag w-screen h-8 items-center flex z-0 fixed top-0">
+
+    </nav>
+  );
+}
+  
+export default TitleBar;
