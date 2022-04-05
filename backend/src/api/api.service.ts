2c56255acb3ea4784f028225d5bf56a690e837fe
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-04 23:23:08 -0400

    Fix openai api 401 error by supplying authorization header.

--- backend/src/api/api.service.ts
+++ backend/src/api/api.service.ts
@@ -3,7 +3,7 @@ import { OpenAIApi } from 'openai';
 
 const TEST_TEXT = "The Guggenheim, short for the Solomon R. Guggenheim museum, is a very famous art museum located at Fifth Avenue in New York City, and is very much known for its collection of impressionist, post-impressionist, modern, and contemporary art, which continues to grow with time. One of the museum’s current exhibitions, titled “Artistic License,” features the curated works for six curated artists and runs from  May 24 2019 January 12th 2020.";
 
-const EXTRACT_KEYWORDS_TEXT = "Generate search keywords for google from this text: \n\n"
+const EXTRACT_KEYWORDS_TEXT = "Generate search questions for google from this text: \n\n"
 
 @Injectable()
 export class ApiService {
@@ -14,11 +14,20 @@ export class ApiService {
         const result = await this.openaiClient.createCompletion('text-ada-001', {
             prompt: EXTRACT_KEYWORDS_TEXT + TEST_TEXT,
             temperature: 0.3,
-            max_tokens: 60,
+            max_tokens: 70,
             top_p: 1.0,
             frequency_penalty: 0.8,
             presence_penalty: 0.0
-        }).catch((err) => console.log(err));
+        }, { headers: { "Authorization": `Bearer ${process.env.OPENAI_API_KEY}` } })
+        // .then((val) => console.log("RESULT: ", val.data.choices[0].text))
+        let generatedKeywords = result.data.choices[0].text.trim().split('\n');
+        let parsedKeywords = [];
+        generatedKeywords.forEach((keyword) => {
+            let match = keyword.match('(?!^\\d.)(?!\\.).*');
+            match.forEach((val) => parsedKeywords.push(val.trim()));
+        })
+        console.log(parsedKeywords)
+        // console.log("RESULT: ", result.data.choices[0].text)
         return [];
     }
 
