d7442a6ffac09af092553e5e5166cbc9561a9c1b
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-09 23:26:34 -0400

    Add google search api.

--- backend/src/api/api.service.ts
+++ backend/src/api/api.service.ts
@@ -1,5 +1,7 @@
+import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
 import { OpenAIApi } from 'openai';
+const google = require('googlethis');
 
 const TEST_TEXT = "The Guggenheim, short for the Solomon R. Guggenheim museum, is a very famous art museum located at Fifth Avenue in New York City, and is very much known for its collection of impressionist, post-impressionist, modern, and contemporary art, which continues to grow with time. One of the museum’s current exhibitions, titled “Artistic License,” features the curated works for six curated artists and runs from  May 24 2019 January 12th 2020.";
 
@@ -8,7 +10,7 @@ const EXTRACT_KEYWORDS_TEXT = "Generate search questions for google from this te
 @Injectable()
 export class ApiService {
 
-    constructor(@Inject('OPENAI_CLIENT') private openaiClient: OpenAIApi) { }
+    constructor(@Inject('OPENAI_CLIENT') private openaiClient: OpenAIApi, private httpService: HttpService) { }
 
     async generateKeywords(textToCheck: string): Promise<string[]> {
         const result = await this.openaiClient.createCompletion('text-ada-001', {
@@ -26,7 +28,21 @@ export class ApiService {
             let match = keyword.match('(?!^\\d.)(?!\\.).*');
             match.forEach((val) => parsedKeywords.push(val.trim()));
         })
+        const options = {
+            page: 0,
+            safe: false, // hide explicit results?
+            additional_params: {
+                // add additional parameters here, see https://moz.com/blog/the-ultimate-guide-to-the-google-search-parameters and https://www.seoquake.com/blog/google-search-param/
+                hl: 'en'
+            }
+        }
+        const response = await google.search(parsedKeywords[0], options);
+        console.log(response);
         console.log(parsedKeywords)
+        // const res = this.httpService.get('https://www.googleapis.com/customsearch/v1?key=AIzaSyAP7YO6G7H7XsZ7K1NPaHxxzetufWyHidg&cx=91d7ea47b9d6d55fa&q=What%20type%20of%20art%20does%20the%20Guggenheim%20have%20in%20its%20collection%3F');
+        // res.subscribe((observer) => {
+        //     console.log(observer.data.items);
+        // })
         // console.log("RESULT: ", result.data.choices[0].text)
         return [];
     }
