6074cf21f6134f8051eeb35059573cc4f8438ec0
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-17 23:07:51 -0400

    Update drawer layout. Change drawer home button and drawer spacing. Add navigation fix to login screen.

--- frontend/packages/renderer/src/components/Topbar/components/Drawer/index.tsx
+++ frontend/packages/renderer/src/components/Topbar/components/Drawer/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useGetAllDocumentsQuery } from "@/redux/api/documentEndpoints";
 import { signOutLocal } from "@/redux/slices/authSlice";
-import { Avatar, Button, Loader, Accordion, AccordionItem, Menu, MenuItem, createStyles, UnstyledButton, Group, Text } from "@mantine/core";
+import { Avatar, Button, Loader, Accordion, AccordionItem, Menu, MenuItem, createStyles, UnstyledButton, Group, Text, Divider } from "@mantine/core";
 import { getAuth, signOut } from "firebase/auth";
 import { HiDocument } from "react-icons/hi";
 import { MdFolder, MdPerson, MdLogout, MdHome, MdChevronRight } from "react-icons/md";
@@ -55,31 +55,32 @@ export const DrawerContent = () => {
         });
     }
     return (
-        <div className="flex flex-col gap-8">
-            <div className="flex flex-row items-center gap-4">
-                <Menu
-                    closeOnItemClick
-                    control={<UserButton documentCount={data ? data.length : 0} userName='Rahul' userImage='' />}
-                >
-                    <MenuItem icon={<MdLogout />} onClick={signUserOut}> Sign out </MenuItem>
-                </Menu>
-            </div>
-            <Button variant='filled' size='lg' onClick={() => navigate('/')} leftIcon={<MdHome color="white" />}> Home </Button>
-            <div className="flex flex-col text-[#797E8A] gap-2">
-                <h1 className="uppercase text-sm font-bold"> Recent </h1>
-                <div className="flex flex-col">
-                    {data && !isLoading ? data.map((val) => <DrawerDocument key={val.id} documentId={val.id} documentName={val.title} />) : <Loader />}
+        <div className="flex flex-col gap-4">
+            <Menu
+                closeOnItemClick
+                control={<UserButton documentCount={data ? data.length : 0} userName='Rahul' userImage='' />}
+            >
+                <MenuItem icon={<MdLogout />} onClick={signUserOut}> Sign out </MenuItem>
+            </Menu>
+            <Link className="flex gap-2 items-center text-lg p-1 text-[#797E8A] hover:text-white hover:bg-gray-600 rounded-sm" to={'/'} replace> <MdHome /> Home </Link>
+            {/* <Button variant='filled' size='lg' onClick={() => navigate('/')} leftIcon={<MdHome color="white" />}> Home </Button> */}
+            <div className="flex flex-col gap-6">
+                <div className="flex flex-col text-[#797E8A] gap-2">
+                    <h1 className="uppercase text-sm font-bold"> Recent </h1>
+                    <div className="flex flex-col">
+                        {data && !isLoading ? data.map((val) => <DrawerDocument key={val.id} documentId={val.id} documentName={val.title} />) : <Loader />}
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-col text-[#797E8A] gap-2">
-                <h1 className="uppercase text-sm font-bold"> Folders </h1>
-                <Accordion>
-                    <AccordionItem label={<AccordionLabel />} styles={{ icon: { color: 'white' }, control: { ":hover": { backgroundColor: '#41444B' }, padding: '4px' } }}>
-                        {/* <DrawerDocument documentName="Random test document" /> */}
-                    </AccordionItem>
-                </Accordion>
-                <div className="flex flex-col">
+                <div className="flex flex-col text-[#797E8A] gap-2">
+                    <h1 className="uppercase text-sm font-bold"> Folders </h1>
+                    <Accordion>
+                        <AccordionItem label={<AccordionLabel />} styles={{ icon: { color: 'white' }, control: { ":hover": { backgroundColor: '#41444B' }, padding: '4px' } }}>
+                            {/* <DrawerDocument documentName="Random test document" /> */}
+                        </AccordionItem>
+                    </Accordion>
+                    <div className="flex flex-col">
 
+                    </div>
                 </div>
             </div>
         </div>
