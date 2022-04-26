946f23e38c1ce6576d6fe4a8fd7c4d9fa34452c1
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-04-25 22:39:29 -0400

    Add rename folder option. Add display for folder documents. Fix update folder name endpoint.

--- frontend/packages/renderer/src/pages/FoldersPage/index.tsx
+++ frontend/packages/renderer/src/pages/FoldersPage/index.tsx
@@ -1,16 +1,19 @@
 import NewFolderModal from '@/components/NewFolderModal';
-import { useGetAllFoldersQuery, useNewFolderMutation } from '@/redux/api/folderEndpoints';
+import { useDeleteFolderMutation, useGetAllFoldersQuery, useNewFolderMutation } from '@/redux/api/folderEndpoints';
 import { IFolder } from '@/types';
-import { Loader, UnstyledButton } from '@mantine/core';
+import { Loader, Menu, MenuItem, UnstyledButton } from '@mantine/core';
+import { useClickOutside } from '@mantine/hooks';
 import { nanoid } from 'nanoid';
 import * as React from 'react';
-import { MdArrowBack, MdCreateNewFolder, MdFolder } from 'react-icons/md';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { MdArrowBack, MdCreateNewFolder, MdDelete, MdFolder, MdShortText } from 'react-icons/md';
+import { Outlet, useNavigate, useOutletContext } from 'react-router-dom';
 import FolderContentPage from '../FolderContentPage';
 
 const FoldersPage = () => {
 
-    const [isFolderModalOpen, setIsFolderModalOpen] = React.useState(false);
+    const [isCreateModalOpen, setIsCreateModalOpen] = React.useState(false);
+    const [isRenameModalOpen, setIsRenameModalOpen] = React.useState(false);
+    const [focusedFolder, setFocusedFolder] = React.useState<IFolder | null>(null);
 
 
     const navigate = useNavigate();
@@ -19,45 +22,56 @@ const FoldersPage = () => {
         navigate(-1);
     }
 
-    const createNewFolder = () => {
-        setIsFolderModalOpen(true);
+    const openFolderModal = () => {
+        setIsCreateModalOpen(true);
+    }
+
+    const renameFolder = (folder: IFolder) => {
+        setFocusedFolder(folder);
+        setIsRenameModalOpen(true);
     }
 
     return (
         <div className="w-full h-full flex flex-col">
-            <NewFolderModal isOpen={isFolderModalOpen} closeModal={() => setIsFolderModalOpen(false)} />
+            <NewFolderModal isOpen={isCreateModalOpen} closeModal={() => setIsCreateModalOpen(false)} type='create' />
+            <NewFolderModal isOpen={isRenameModalOpen} closeModal={() => setIsRenameModalOpen(false)} currentName={focusedFolder?.name} folderId={focusedFolder?.id} type={'rename'} />
             <div className="w-full h-16 flex items-center justify-between">
                 <UnstyledButton className="text-[#646470] bg-[#38383f] p-2 rounded-md hover:bg-[#3071E8] hover:text-white" onClick={goBack}>
                     <MdArrowBack size={30} />
                 </UnstyledButton>
 
-                <UnstyledButton className="text-white bg-[#3071E8] py-2 px-4 rounded-base flex gap-4 items-center hover:bg-[#4485ff]" onClick={createNewFolder}>
+                <UnstyledButton className="text-white bg-[#3071E8] py-2 px-4 rounded-base flex gap-4 items-center hover:bg-[#4485ff]" onClick={openFolderModal}>
                     <MdCreateNewFolder size={25} />
                     New Folder
                 </UnstyledButton>
             </div>
 
-            <div className="mt-6">
-                <Outlet />
+            <div className="mt-6 relative">
+                <Outlet context={{ renameFolder }} />
             </div>
         </div>
     );
 }
 
+interface IOutletContext {
+    renameFolder: (folder: IFolder) => void
+}
+
 const FoldersHome = () => {
+
     const { data, isLoading, isFetching } = useGetAllFoldersQuery();
 
-    React.useEffect(() => {
-        console.log("Folders data changed: ", data);
-    }, [data])
     return (
-        <div className="w-full h-full grid grid-cols-[repeat(auto-fill,20rem)] gap-8">
-            {(isLoading || isFetching) ? <Loader /> : data && data.map((value) =>
+        <>
 
-                <FolderItem key={value.id} folderTitle={value.name} folderId={value.id} />
-            )
-            }
-        </div>
+            <div className="w-full h-full grid grid-cols-[repeat(auto-fill,20rem)] gap-8">
+                {(isLoading || isFetching) ? <Loader /> : data && data.map((value) =>
+
+                    <FolderItem key={value.id} folderInfo={value} />
+                )
+                }
+            </div>
+        </>
     );
 
 }
@@ -68,25 +82,58 @@ FoldersPage.Home = FoldersHome;
 export default FoldersPage;
 
 interface FolderItemProps {
-    folderTitle: string,
-    folderId: string
+    folderInfo: IFolder
 }
 
-const FolderItem = ({ folderTitle, folderId }: FolderItemProps) => {
+const FolderItem = ({ folderInfo }: FolderItemProps) => {
+
 
     const navigate = useNavigate();
+    const [deleteFolderMutation] = useDeleteFolderMutation();
+    const { renameFolder } = useOutletContext<IOutletContext>();
+
+    const deleteFolder = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();
+        if (folderInfo.id) {
+            deleteFolderMutation(folderInfo.id);
+        }
+    }
 
     const openFolder = () => {
-        navigate(folderId);
+        navigate(folderInfo.id);
     }
 
     return (
-        <button
-            className="w-full max-w-xs h-fit max-h-32 p-4 flex gap-4 text-[#6A6A6A] text-lg items-center rounded-sm border-[1px] border-[#44444A] hover:text-white hover:border-[#3071E8]"
+        <div
+            className="w-full max-w-xs h-fit max-h-32 p-4 flex gap-4 cursor-pointer text-[#6A6A6A] text-lg items-center rounded-sm border-[1px] border-[#44444A] hover:text-white hover:border-[#3071E8]"
             onClick={openFolder}
         >
             <MdFolder />
-            <h6> {folderTitle} </h6>
-        </button>
+            <div className="w-full flex justify-between items-center">
+                <h6> {folderInfo.name.toString()} </h6>
+                <Menu
+                    onClick={(e) => e.stopPropagation()}
+                    styles={{ body: { backgroundColor: '#34343A', border: 'none' }, itemLabel: { color: 'white' }, item: { ':hover': { background: '#45454D' } } }}
+                >
+                    <MenuItem icon={<MdShortText color="white" size={20} />} onClick={() => renameFolder(folderInfo)}> Rename Folder </MenuItem>
+                    <MenuItem icon={<MdDelete color="#F64646" size={20} />} onClick={deleteFolder}> Delete Folder </MenuItem>
+                </Menu>
+            </div>
+        </div>
     );
 }
+
+{/* 
+
+CONTEXT MENU
+
+<Menu
+opened={contextCoords !== null}
+ref={ref}
+control={<div style={{ display: 'none' }}> </div>}
+withinPortal={false}
+styles={{ body: { backgroundColor: '#34343A', border: 'none' }, itemLabel: { color: 'white' }, item: { ':hover': { background: '#45454D' } } }}
+style={{ position: 'fixed', top: contextCoords?.y + 'px', left: contextCoords?.x + 'px' }}>
+<MenuItem icon={<MdShortText color="white" size={20} />} onClick={renameFolder}> Rename Folder </MenuItem>
+<MenuItem icon={<MdDelete color="#F64646" size={20} />} onClick={deleteFolder}> Delete Folder </MenuItem>
+</Menu> */}
